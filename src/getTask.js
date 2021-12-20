888edd15148bb5052efe70e1d7441fadb8bdb34c
gusLopezC <guslopezcallejas@gmail.com>
2021-12-20 07:56:21 -0600

    Obtener una tarea especifica

---
+++ src/getTask.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk");
+
+const getTask = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const { id } = event.pathParameter;
+
+  const result = await dynamodb
+    .get({
+      TableName: "TaskTable",
+      Key: {
+        id,
+      },
+    })
+    .promise();
+};
+
+const task = result.Items;
+
+return {
+  status: 200,
+  body: JSON.stringify(task),
+};
+
+module.exports = {
+  getTask,
+};
--- src/getTasks.js
+++ src/getTasks.js
@@ -1,24 +1,22 @@
 const AWS = require("aws-sdk");
 
 const getTasks = async (event) => {
-  try {
-    const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-    const result = await dynamodb
-      .scan({
-        TableName: "TaskTable",
-      })
-      .promise();
+  const result = await dynamodb
+    .scan({
+      TableName: "TaskTable",
+    })
+    .promise();
 
-    const tasks = result.Items;
+  console.log(result);
 
-    return {
-      statusCode: 200,
-      body: tasks,
-    };
-  } catch (error) {
-    console.log(error);
-  }
+  const tasks = result.Items;
+
+  return {
+    status: 200,
+    body: tasks,
+  };
 };
 
 module.exports = {
